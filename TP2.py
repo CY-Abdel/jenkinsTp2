936415723d4173e3f0fa86db550ffc36243e0627
Abdelhadi Chougar <chougar.abdelhadi@gmail.com>
2023-11-14 13:51:16 +0100

    first commit tp2.py

---
+++ TP2.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+# Obtenir la date et l'heure actuelles
+now = datetime.now()
+
+# Extraire les composants de la date et de l'heure
+mm = now.month
+dd = now.day
+yyyy = now.year
+hour = now.hour
+mi = now.minute
+ss = now.second
+
+# Afficher les informations dans un seul print
+print(f"Date et heure actuelles: {mm}/{dd}/{yyyy} {hour}:{mi}:{ss}")
